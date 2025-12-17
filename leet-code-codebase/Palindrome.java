1e8a49069a2571cd9954a889d5349fddbe673e40
Priyanka33591 <priyanka33591@gmail.com>
2025-12-17 15:21:56 +0530

    add palindrome code

---
+++ leet-code-codebase/Palindrome.java
@@ -0,0 +1,15 @@
+class Palindrome {
+    public static void main(String[] args) {
+        int num = 121, temp = num, rev = 0;
+
+        while (temp != 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        if (num == rev)
+            System.out.println("Palindrome Number");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
