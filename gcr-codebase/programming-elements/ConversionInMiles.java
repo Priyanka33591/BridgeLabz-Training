7058ad4cfad4548afce392f11a5313c01c535061
Priyanka33591 <priyanka33591@gmail.com>
2025-12-18 11:09:47 +0530

    add the program of conversion in miles

---
+++ gcr-codebase/programming-elements/ConversionInMiles.java
@@ -0,0 +1,7 @@
+public class ConversionInMiles {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 0.621371;
+        System.out.println("The distance " + kilometers + " kilometers is equal to " + miles + " miles.");
+    }
+}
