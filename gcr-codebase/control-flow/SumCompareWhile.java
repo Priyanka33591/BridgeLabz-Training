b2041fe9c2cd1e9f4d1ea206e62b0c308f3809b7
Priyanka33591 <priyanka33591@gmail.com>
2025-12-19 15:37:45 +0530

    add SumCompareWhile program to compare the sum of numbers using formula and while loop

---
+++ gcr-codebase/control-flow/SumCompareWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumCompareWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n >= 0) {
+            int sum1 = n * (n + 1) / 2;
+            int sum2 = 0, i = 1;
+
+            while (i <= n) {
+                sum2 += i;
+                i++;
+            }
+
+            System.out.println("Formula Result = " + sum1);
+            System.out.println("While Loop Result = " + sum2);
+        }
+    }
+}
