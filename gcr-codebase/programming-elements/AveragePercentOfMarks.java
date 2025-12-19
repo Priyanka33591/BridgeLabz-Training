8838dd16107b2158eb63074094a3e1e6440205fb
Priyanka33591 <priyanka33591@gmail.com>
2025-12-19 11:21:15 +0530

    refactor AveragePercentOfMarks variable names to lowercase; remove unused SquareSide class

--- gcr-codebase/programming-elements/AveragePercentOfMarks.java
+++ gcr-codebase/programming-elements/AveragePercentOfMarks.java
@@ -1,9 +1,9 @@
 public class AveragePercentOfMarks {
     public static void main(String[] args) {
-        int Maths = 94;
-        int Physics = 95;
-        int Chemistry = 96;
-        int totalMarks = Maths + Physics + Chemistry;
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        int totalMarks = maths + physics + chemistry;
         double averageMarks = (totalMarks / 3.0);
         System.out.println("Sam's average marks in PCM is : " + averageMarks);
     }
