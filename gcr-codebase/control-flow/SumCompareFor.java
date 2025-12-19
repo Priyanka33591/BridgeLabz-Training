91af14836362a337066b67161ababf61f70746b3
Priyanka33591 <priyanka33591@gmail.com>
2025-12-19 15:42:27 +0530

    add EmployeeBonus, FactorialFor, FactorialWhile, OddEven, SumCompareFor, and Table6to9 programs for various calculations and outputs

---
+++ gcr-codebase/control-flow/SumCompareFor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumCompareFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n >= 0) {
+            int sum1 = n * (n + 1) / 2;
+            int sum2 = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sum2 += i;
+            }
+
+            System.out.println("Formula Result = " + sum1);
+            System.out.println("For Loop Result = " + sum2);
+        }
+    }
+}
