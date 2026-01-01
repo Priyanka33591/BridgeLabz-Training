5b6072103f0bba0f1eb1598da250ad6945baa66c
Priyanka33591 <priyanka33591@gmail.com>
2026-01-01 11:52:47 +0530

    [Priyanka] addcode of  Implement First and Last Index Search using Binary Search

---
+++ FirstAndLastIndexUsingBinarySearch.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class FirstAndLastIndexUsingBinarySearch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int len = scan.nextInt();
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = scan.nextInt();
+        }
+        int target = scan.nextInt();
+        int firstIdx = firstIndex(arr, target);
+        int lastIdx = lastIndex(arr, target);
+        System.out.println("first index is " + firstIdx);
+        System.out.println("last index is " + lastIdx);
+    }
+
+    public static int firstIndex(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int firstIdx = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                firstIdx = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return firstIdx;
+    }
+
+    public static int lastIndex(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int lastIdx = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                lastIdx = mid;
+                left = mid + 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return lastIdx;
+    }
+}
