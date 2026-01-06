38c315b039c6c4303d579bbdcae1d907ec39e621
Priyanka33591 <priyanka33591@gmail.com>
2026-01-06 11:05:17 +0530

    [PRIYANKA] Add QuizProcessor class with score calculation and grading functionality

---
+++ oops-practice/scenario-Based/QuizPlatform.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class InvalidQuizSubmissionException extends Exception {
+    public InvalidQuizSubmissionException(String msg) {
+        super(msg);
+    }
+}
+
+class QuizProcessor {
+
+    static int calculateScore(String[] correct, String[] user)
+            throws InvalidQuizSubmissionException {
+
+        if (correct.length != user.length) {
+            throw new InvalidQuizSubmissionException("Answer length mismatch");
+        }
+
+        int score = 0;
+        for (int i = 0; i < correct.length; i++) {
+            if (correct[i].equalsIgnoreCase(user[i])) {
+                score++;
+            }
+        }
+        return score;
+    }
+
+    static String getGrade(int score, int total) {
+        double percent = (score * 100.0) / total;
+
+        if (percent >= 80) return "A";
+        if (percent >= 60) return "B";
+        if (percent >= 40) return "C";
+        return "Fail";
+    }
+}
