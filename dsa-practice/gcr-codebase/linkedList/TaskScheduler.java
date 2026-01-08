cc7086ca07fdbfdc5fa1712726549ce9483efb28
Priyanka33591 <priyanka33591@gmail.com>
2026-01-08 12:12:48 +0530

    [Priyanka] Refactor TaskNode and TaskScheduler classes to remove dueDate attribute and simplify constructor

--- dsa-practice/gcr-codebase/linkedList/TaskScheduler.java
+++ dsa-practice/gcr-codebase/linkedList/TaskScheduler.java
@@ -1,19 +1,17 @@
 class TaskNode {
-    int id;
-    String name, dueDate;
-    int priority;
+    int id, priority;
+    String name;
     TaskNode next;
 
-    TaskNode(int id, String name, int priority, String dueDate) {
+    TaskNode(int id, String name, int priority) {
         this.id = id;
         this.name = name;
         this.priority = priority;
-        this.dueDate = dueDate;
     }
 }
 
-class TaskScheduler {
-    TaskNode head = null;
+public class TaskScheduler {
+    TaskNode head;
 
     void addTask(TaskNode node) {
         if (head == null) {
@@ -35,5 +33,12 @@ class TaskScheduler {
             System.out.println(temp.name);
             temp = temp.next;
         } while (temp != head);
+    }
+
+    public static void main(String[] args) {
+        TaskScheduler t = new TaskScheduler();
+        t.addTask(new TaskNode(1, "Coding", 1));
+        t.addTask(new TaskNode(2, "Testing", 2));
+        t.display();
     }
 }
