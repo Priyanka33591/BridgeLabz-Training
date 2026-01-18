63642326f20085987d277662c6e643f711a880f0
Priyanka33591 <priyanka33591@gmail.com>
2026-01-18 22:34:40 +0530

    [Priyanka] Implement FirstLast class to find the first and last occurrence of a target in a sorted array

---
+++ dsa-practice/gcr-codebase/searching/FirstLast.java
@@ -0,0 +1,23 @@
+public class FirstLast {
+    static int first(int[] a, int t) {
+        int l = 0, r = a.length - 1, res = -1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (a[m] == t) { res = m; r = m - 1; }
+            else if (a[m] < t) l = m + 1;
+            else r = m - 1;
+        }
+        return res;
+    }
+
+    static int last(int[] a, int t) {
+        int l = 0, r = a.length - 1, res = -1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (a[m] == t) { res = m; l = m + 1; }
+            else if (a[m] < t) l = m + 1;
+            else r = m - 1;
+        }
+        return res;
+    }
+}
