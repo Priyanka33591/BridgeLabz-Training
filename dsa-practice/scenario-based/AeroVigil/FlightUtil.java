d6073d3e243aabf15af2ad8f2d06a5798a933a3c
Priyanka33591 <priyanka33591@gmail.com>
2026-02-05 23:47:38 +0530

    [Priyanka] Add FlightUtil and InvalidFlightException classes for flight validation and fuel calculation

---
+++ dsa-practice/scenario-based/AeroVigil/FlightUtil.java
@@ -0,0 +1,64 @@
+import java.util.regex.*;
+
+public class FlightUtil {
+
+    public boolean validateFlightNumber(String flightNumber) throws InvalidFlightException {
+        String pattern = "FL-[1-9][0-9]{3}";
+
+        if (!Pattern.matches(pattern, flightNumber)) {
+            throw new InvalidFlightException("The flight number " + flightNumber + " is invalid");
+        }
+
+        return true;
+    }
+
+    public boolean validateFlightName(String flightName) throws InvalidFlightException {
+
+        if (!(flightName.equals("SpiceJet") ||
+              flightName.equals("Vistara") ||
+              flightName.equals("IndiGo") ||
+              flightName.equals("Air Arabia"))) {
+
+            throw new InvalidFlightException("The flight name " + flightName + " is invalid");
+        }
+
+        return true;
+    }
+
+    public boolean validatePassengerCount(int passengerCount, String flightName) throws InvalidFlightException {
+
+        int maxCapacity = 0;
+
+        switch (flightName) {
+            case "SpiceJet": maxCapacity = 396; break;
+            case "Vistara": maxCapacity = 615; break;
+            case "IndiGo": maxCapacity = 230; break;
+            case "Air Arabia": maxCapacity = 130; break;
+        }
+
+        if (passengerCount <= 0 || passengerCount > maxCapacity) {
+            throw new InvalidFlightException(
+                    "The passenger count " + passengerCount + " is invalid for " + flightName);
+        }
+
+        return true;
+    }
+
+    public double calculateFuelToFillTank(String flightName, double currentFuelLevel) throws InvalidFlightException {
+
+        double capacity = 0;
+
+        switch (flightName) {
+            case "SpiceJet": capacity = 200000; break;
+            case "Vistara": capacity = 300000; break;
+            case "IndiGo": capacity = 250000; break;
+            case "Air Arabia": capacity = 150000; break;
+        }
+
+        if (currentFuelLevel < 0 || currentFuelLevel > capacity) {
+            throw new InvalidFlightException("Invalid fuel level for " + flightName);
+        }
+
+        return capacity - currentFuelLevel;
+    }
+}
