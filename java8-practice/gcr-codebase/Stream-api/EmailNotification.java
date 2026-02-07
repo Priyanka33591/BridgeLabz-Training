1fa3a79146f711e27407f793ac86c8eac6538d55
Priyanka33591 <priyanka33591@gmail.com>
2026-02-07 22:24:43 +0530

    [Priyanka] Add EmailNotification class to handle sending email notifications

---
+++ java8-practice/gcr-codebase/Stream-api/EmailNotification.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class EmailNotification {
+
+    static void sendEmailNotification(String email) {
+        System.out.println("Email sent to " + email);
+    }
+
+    public static void main(String[] args) {
+
+        List<String> emails = List.of("a@gmail.com", "b@gmail.com");
+        emails.forEach(EmailNotification::sendEmailNotification);
+    }
+}
