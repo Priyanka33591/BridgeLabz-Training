d4071f6baf23d449ac3a78701ecae84410f11595
Priyanka33591 <priyanka33591@gmail.com>
2026-02-09 11:05:04 +0530

    [Priyanka] Add whitespace for improved code readability in BackgroundJobRunnable

--- java8-practice/gcr-codebase/Functional-Interface/BackgroundJobRunnable.java
+++ java8-practice/gcr-codebase/Functional-Interface/BackgroundJobRunnable.java
@@ -1,6 +1,7 @@
 public class BackgroundJobRunnable {
     public static void main(String[] args) {
 
+        
         Runnable job = () ->
             System.out.println("Background Job Running");
 
